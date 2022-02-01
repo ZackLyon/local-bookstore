085d2ec8b6a65f7e690f3b3fac32238651bec0ad
KDSigel <heykarl@gmail.com>
2022-01-31 17:44:34 -0800

    added test to post authors, test is failing

---
+++ __tests__/authors.test.js
@@ -0,0 +1,28 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Art backend routes', () => {
+  beforeEach(async () => {
+    await setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('add an author if we do a post', async () => {
+    const res = await request(app).post('/api/v1/authors/').send({
+      name: 'Ms. Person',
+      dob: '1945-02-14',
+      pod: 'Brazil',
+    });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Ms. Person',
+      dob: '1945-02-14',
+      pod: 'Brazil',
+    });
+  });
+});
