eb4a7f3dbccdd0a7b8ffc8d0f48f2c1e6c761052
AmitSheth9 <amit.sheth9@gmail.com>
2022-01-31 18:56:02 -0600

    wrote reviewers controller, Reviewer model and corresponding tests for all routes

---
+++ __tests__/reviewer.test.js
@@ -0,0 +1,79 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Reviewer = require('../lib/models/Reviewer')
+
+describe('backend routes', () => {
+    beforeEach(() => {
+      return setup(pool);
+    });
+  
+    afterAll(() => {
+      pool.end();
+    });
+
+    const testReviewer = {
+        name: 'jeeves',
+        company: 'askjeeves'
+    };
+
+    const expectedReviewer = {
+        id: expect.any(String),
+        name: 'jeeves',
+        company: 'askjeeves'
+    };
+
+    it('should post a reviewer', async () => {
+        const res = await request(app)
+        .post('/reviewers')
+        .send(testReviewer);
+
+        expect(res.body).toEqual(expectedReviewer)
+    });
+    it('should get all reviewers', async () => {
+        await Reviewer.insert(testReviewer);
+        const res = await request(app)
+        .get('/reviewers');
+
+        expect(res.body).toEqual([{
+            id: expect.any(String),
+            name:'Amit Just Amit',
+            company: 'Books-R-US'
+        },
+        {
+        id:expect.any(String),
+        name: 'jeeves',
+        company: 'askjeeves'
+        }
+        ]);
+    });
+    it('should get a reviewer by id', async () => {
+        const reviewer = await Reviewer.insert(testReviewer);
+        const res = await request(app)
+        .get(`/reviewers/${reviewer.id}`);
+
+        expect(res.body).toEqual(reviewer);
+    });
+    it('should update a reviewer by id', async ()=> {
+        const reviewer = await Reviewer.insert(testReviewer);
+        const res = await request(app)
+        .patch(`/reviewers/${reviewer.id}`)
+        .send({ name: 'ronald'});
+
+        const expected = {
+            id: reviewer.id,
+            name: 'ronald',
+            company: 'askjeeves'
+        };
+        expect(res.body).toEqual(expected);
+    });
+    it('should delete a reviewer by id', async () => {
+        const reviewer = await Reviewer.insert(testReviewer);
+        const res = await request(app)
+        .delete(`/reviewers/${reviewer.id}`);
+
+        expect(await Reviewer.getById(reviewer.id)).toBeNull();
+    })
+
+});
