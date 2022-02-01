c0c776da25feaa85bd439307fa6b1f6439b789f6
KDSigel <heykarl@gmail.com>
2022-01-31 18:41:03 -0800

    added test for getting all authors. test is failing

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Author = require('../lib/models/Author');
 
 describe('Art backend routes', () => {
   beforeEach(async () => {
@@ -19,6 +20,22 @@ describe('Art backend routes', () => {
       pob: 'Brazil',
     });
     expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Ms. Person',
+      dob: '1945-02-14T07:00:00.000Z',
+      pob: 'Brazil',
+    });
+  });
+
+  it('gets all authors when I do a get', async () => {
+    await Author.insert({
+      name: 'Ms. Person',
+      dob: '1945-02-14',
+      pob: 'Brazil',
+    });
+    const res = await request(app).get('/api/v1/authors');
+
+    expect(res.body).toContainEqual({
       id: expect.any(String),
       name: 'Ms. Person',
       dob: '1945-02-14T07:00:00.000Z',
