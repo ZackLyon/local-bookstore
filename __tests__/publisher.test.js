5c01cd87da15e31789529bf39ed9f6485df90b55
EmilyDSarani <emily.sarani@gmail.com>
2022-01-31 18:57:10 -0600

    get all done

--- __tests__/publisher.test.js
+++ __tests__/publisher.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Publisher = require('../lib/services/Publisher');
 
 describe('the backend routes', () => {
     beforeEach(() => {
@@ -19,6 +20,18 @@ describe('the backend routes', () => {
          expect(res.body).toEqual({ id: expect.any(String), name: 'BetweentheLyons', city: 'Portland', state: 'Oregon', country: 'United State of America' });
         });
 
+        it('should get all publisers', async() => {
+            await Publisher.insert({
+                name: 'BetweentheLyons', city: 'Portland', state: 'Oregon', country: 'United State of America'   
+            });
+        
+            const res = await request(app).get('/api/v1/publishers');
+            expect(res.body).toEqual([{ id: expect.any(String), name: 'Sarani Inc', city: 'OKC', state: 'OK', country: 'USA'  
+            },
+            { id: expect.any(String), name: 'BetweentheLyons', city: 'Portland', state: 'Oregon', country: 'United State of America'}]);
+          });
+
+          
 
 
 })
